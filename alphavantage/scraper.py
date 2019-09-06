557fa17ecd2474d26118e7c3bf790a588b8fe96b
Charlie Chen <ccehshmily@gmail.com>
2019-09-05 21:53:26 -0700

    Change intent to 2 spaces

--- alphavantage/scraper.py
+++ alphavantage/scraper.py
@@ -7,32 +7,29 @@ from constants import *
 SYMBOLS = ['MSFT']
 FUNCTIONS = ['TIME_SERIES_INTRADAY']
 FUNCTION_CONFIGS = {
-    'TIME_SERIES_INTRADAY': {
-        INTERVAL: '5min',
-    }
+  'TIME_SERIES_INTRADAY': {
+    INTERVAL: '5min',
+  }
 }
 
 
-def _build_configs(symbol, function):
+class Scraper():
+  def scrape():
+    for symbol in SYMBOLS:
+      for function in FUNCTIONS:
+        config = self._build_configs(symbol, function)
+        url = self._build_url(config)
+        response = requests.get(url)
+
+  def _build_configs(self, symbol, function):
     config = {
-        SYMBOL: symbol,
-        FUNCTION: function,
-        API_KEY: ALPHA_VANTAGE_API_KEY,
+      SYMBOL: symbol,
+      FUNCTION: function,
+      API_KEY: ALPHA_VANTAGE_API_KEY,
     }
     config.update(FUNCTION_CONFIGS[function])
     return config
 
-
-def _build_url(config):
+  def _build_url(self, config):
     url = BASE_URL + '&'.join([key + '=' + config[key] for key in config])
     return url
-
-
-def scrape():
-    for symbol in SYMBOLS:
-        for function in FUNCTIONS:
-            config = _build_configs(symbol, function)
-            url = _build_url(config)
-            response = requests.get(url)
-
-scrape()
