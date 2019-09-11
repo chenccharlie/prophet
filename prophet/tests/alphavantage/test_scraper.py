a73588c63796196598bcbf3697105a93acfdf1a4
Charlie Chen <ccehshmily@gmail.com>
2019-09-10 21:56:53 -0700

    fix tests

--- prophet/tests/alphavantage/test_scraper.py
+++ prophet/tests/alphavantage/test_scraper.py
@@ -3,11 +3,19 @@ import unittest
 from prophet.alphavantage.scraper import Scraper
 
 class TestScraper(unittest.TestCase):
+  LABELS = [
+    "open",
+    "high",
+    "low",
+    "close",
+    "volume",
+  ]
+
   def setUp(self):
     self.scraper = Scraper()
 
   def test_scrape(self):
-      self.assertEqual(self.scraper.scrape("MSFT"), 'FOO')
+      self.assertEqual(self.scraper.scrape("MSFT").data.keys(), LABELS)
 
 if __name__ == '__main__':
     unittest.main()
